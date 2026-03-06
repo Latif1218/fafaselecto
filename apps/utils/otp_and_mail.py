ec56159a9f3f87ca2e460f8a97a47ea69f2c96a3
Latif1218 <mdsabdullotif@gmail.com>
2026-03-06 11:33:25 +0600

    add otp generate and otp sender file

---
+++ apps/utils/otp_and_mail.py
@@ -0,0 +1,46 @@
+import smtplib
+import secrets
+from fastapi import HTTPException, status
+from email.message import EmailMessage
+from ..config import EMAIL_FROM, EMAIL_HOST, EMAIL_PASSWORD, EMAIL_PORT, EMAIL_USE_TLS, EMAIL_USER
+from smtplib import SMTPException, SMTPAuthenticationError
+
+
+def generate_otp() -> str:
+    return f"{secrets.randbelow(1_000_000): 06d}"
+
+
+def send_otp_email(to_email: str, otp: str) -> bool:
+    
+    subject = "Your IRON READY OTP Code"
+    body = f"""
+    Hi,
+
+    Your OTP code is: {otp}
+
+    If you did not fequest this code, pleace ignore this email.
+
+    best regards,
+    POSTULAE
+    """
+
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_FROM
+    msg["to"] = to_email
+    msg.set_content(body)
+
+    try:
+        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+        if EMAIL_USE_TLS:
+            server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.send_message(msg)
+        server.quit()
+        return True
+    
+    except (SMTPException, SMTPAuthenticationError, ConnectionError) as exc:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Failed to send OTP email"
+        ) from exc
